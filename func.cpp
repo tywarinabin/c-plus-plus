2f8464585bc6f288b0afb055ea0dde739e29e907
Nabin Tiwari <96787565+tywarinabin@users.noreply.github.com>
2022-09-08 23:55:55 +0530

    Basic of Function Greet using cpp

---
+++ func.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void greet();   // Function Prototype
+int main(){
+  greet(); // Function Call
+return 0;
+}
+void greet(){   // Function Defination
+  cout<<"HEY"<<endl;
+    cout<<"Hello , Good Morning"<<endl;
+}
